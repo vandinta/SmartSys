23f8ea8a0de761d454b541648c501a0e70671ea4
vandinta <89644669+vandinta@users.noreply.github.com>
2023-01-30 19:29:29 +0700

    Add Migrate

--- app/Database/Migrations/2023-01-30-112308_Barang.php
+++ app/Database/Migrations/2023-01-30-112308_Barang.php
@@ -7,12 +7,55 @@ use CodeIgniter\Database\Migration;
 class Barang extends Migration
 {
     public function up()
-    {
-        //
-    }
+	{
+		$this->forge->addField([
+			'id_barang'          => [
+				'type'           => 'INT',
+				'constraint'     => 5,
+				'unsigned'       => true,
+				'auto_increment' => true
+			],
+            'id_kategori'          => [
+				'type'           => 'INT',
+				'constraint'     => 5,
+				'unsigned'       => true,
+				'null'			=> false,
+			],
+			'nama_barang'       => [
+				'type'           => 'VARCHAR',
+				'constraint'     => 255,
+				'null'           => false,
+			],
+			'stok_barang'       => [
+				'type'           => 'VARCHAR',
+				'constraint'     => 5,
+				'null'           => false,
+			],
+            'harga_beli'       => [
+				'type'           => 'VARCHAR',
+				'constraint'     => 255,
+				'null'           => false,
+			],
+            'harga_jual'       => [
+				'type'           => 'VARCHAR',
+				'constraint'     => 255,
+				'null'           => false,
+			],
+			'image_barang'      => [
+				'type'           => 'VARCHAR',
+				'constraint'     => 255,
+			],
+			'created_at datetime default current_timestamp',
+			'updated_at datetime default current_timestamp on update current_timestamp',
+		]);
 
-    public function down()
-    {
-        //
-    }
+		$this->forge->addKey('id_barang', TRUE);
+        $this->forge->addForeignKey('id_kategori', 'tb_kategori', 'id_kategori', 'NO ACTION', 'NO ACTION');
+		$this->forge->createTable('tb_barang', TRUE);
+	}
+
+	public function down()
+	{
+		$this->forge->dropTable('tb_barang');
+	}
 }
