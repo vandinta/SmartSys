9b992d98e6ff1e24be8e6e7244865ab7b8977cd8
vandinta <89644669+vandinta@users.noreply.github.com>
2023-01-31 16:43:16 +0700

    Login & Layouting Dashboard

--- app/Models/UsersModel.php
+++ app/Models/UsersModel.php
@@ -8,35 +8,17 @@ class UsersModel extends Model
 {
     protected $DBGroup          = 'default';
     protected $table            = 'users';
-    protected $primaryKey       = 'id';
+    protected $primaryKey       = 'user_id';
     protected $useAutoIncrement = true;
     protected $insertID         = 0;
     protected $returnType       = 'array';
     protected $useSoftDeletes   = false;
     protected $protectFields    = true;
-    protected $allowedFields    = [];
+    protected $allowedFields    = ['email', 'password', 'username', 'role', 'profile_picture', 'activation_status', 'created_at', 'updated_at'];
 
     // Dates
     protected $useTimestamps = false;
     protected $dateFormat    = 'datetime';
     protected $createdField  = 'created_at';
     protected $updatedField  = 'updated_at';
-    protected $deletedField  = 'deleted_at';
-
-    // Validation
-    protected $validationRules      = [];
-    protected $validationMessages   = [];
-    protected $skipValidation       = false;
-    protected $cleanValidationRules = true;
-
-    // Callbacks
-    protected $allowCallbacks = true;
-    protected $beforeInsert   = [];
-    protected $afterInsert    = [];
-    protected $beforeUpdate   = [];
-    protected $afterUpdate    = [];
-    protected $beforeFind     = [];
-    protected $afterFind      = [];
-    protected $beforeDelete   = [];
-    protected $afterDelete    = [];
 }
---
+++ app/Views/cms/layout/v_template.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<title>Dashboard - SmartSys</title>
+		<meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
+		<?= $this->include("cms/layout/v_header") ?>
+	</head>
+	<body>
+		<div class="wrapper">
+			<!-- Navbar -->
+			<?= $this->include("cms/layout/v_navbar") ?>
+			<!-- End Navbar -->
+
+			<!-- Sidebar -->
+			<?= $this->include("cms/layout/v_sidebar") ?>
+			<!-- End Sidebar -->
+
+			<div class="main-panel">
+				<div class="content">
+					<?= $this->renderSection("content") ?>
+				</div>
+
+				<!--   Footer  -->
+				<?= $this->include("cms/layout/v_footer") ?>
+				<!--   Footer End  -->
+			</div>
+		</div>
+
+		<!--  JS  -->
+		<?= $this->include("cms/layout/v_js") ?>
+		<!--  JS End  -->
+	</body>
+</html>
