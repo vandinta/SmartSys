56cb7ba7d264a6313b57bd6a0cd2854e63d2a30f
vandinta <89644669+vandinta@users.noreply.github.com>
2023-02-18 14:17:59 +0700

    Option Selected Penjualan

--- app/Views/cms/penjualan/v_tambahdata.php
+++ app/Views/cms/penjualan/v_tambahdata.php
@@ -37,25 +37,29 @@
               <thead>
                 <tr>
                   <th scope="col">Barang</th>
-                  <th scope="col">Jumlah</th>
                   <th scope="col">Harga</th>
+                  <th scope="col">Banyak</th>
+                  <th scope="col">Jumlah</th>
                   <th style="width: 3%; text-align:center;">Aksi</th>
                 </tr>
               </thead>
               <tbody class="formtambah">
                 <tr>
                   <td>
-                    <select class="form-control" style="margin: 10px;" id="id_barang" name="id_barang[]">
+                    <select class="form-control" style="margin: 10px;" id="id_barang" name="id_barang" onchange="Hitung(this);">
                       <?php foreach ($barang as $brg) : ?>
                         <option data-harga="<?= $brg['harga_jual'] ?>" value="<?= $brg['id_barang'] ?>"><?= $brg['nama_barang'] ?></option>
                       <?php endforeach; ?>
                     </select>
                   </td>
                   <td>
-                    <input type="number" class="form-control" id="jumlah_barang" name="jumlah_barang[]" placeholder="Jumlah">
+                    <input type="text" class="form-control" id="harga_barang" name="harga_barang" disabled>
                   </td>
                   <td>
-                    <input type="text" class="form-control" id="jumlah_harga_barang" name="jumlah_harga_barang[]" disabled>
+                    <input type="number" class="form-control" id="jumlah_barang" name="jumlah_barang" onchange="Hitung(this);" placeholder="Jumlah">
+                  </td>
+                  <td>
+                    <input type="text" class="form-control" id="jumlah_harga_barang" name="jumlah_harga_barang" disabled>
                   </td>
                   <td>
                     <button type="button" data-toggle="tooltip" title="" class="btn btn-outline-primary addform" data-original-title="Tambah">
@@ -65,6 +69,13 @@
                 </tr>
               </tbody>
             </table>
+            <div class="col-md-2 col-lg-3">
+              <div class="coloum">
+                <h4>Total Harga</h4>
+                <input type="text" class="form-control" id=" " name=" " disabled>
+              </div>
+            </div>
+            <br><br>
             <div>
               <button type="submit" class="btn btn-outline-success float-right mr-2 simpandata">Simpan</button>
               <a href="<?php echo base_url('/datapenjualan') ?>" type="button" class="btn btn-outline-danger float-right mr-2">Batal</a>
@@ -83,7 +94,7 @@
     $('.addform').click(function(e) {
       e.preventDefault();
 
-      var html = '<tr><td><select class="form-control" style="margin: 10px;" id="id_barang" name="id_barang[]"><?php foreach ($barang as $brg) : ?><option data-harga="<?= $brg['harga_jual'] ?>" value="<?= $brg['id_barang'] ?>"><?= $brg['nama_barang'] ?></option><?php endforeach; ?></select></td><td><input type="number" class="form-control" id="jumlah_barang" name="jumlah_barang[]" placeholder="Jumlah"></td><td><input type="text" class="form-control" id="jumlah_harga_barang" name="jumlah_harga_barang[]" disabled></td><td><button type="button" data-toggle="tooltip" title="" class="btn btn-outline-danger deleteform" data-original-title="Hapus"><i class="fa fa-trash"></i></button></td></tr>';
+      var html = '<tr><td><select class="form-control" style="margin: 10px;" id="id_barang" name="id_barang" onchange="Hitung(this);"><?php foreach ($barang as $brg) : ?><option data-harga="<?= $brg['harga_jual'] ?>" value="<?= $brg['id_barang'] ?>"><?= $brg['nama_barang'] ?></option><?php endforeach; ?></select></td><td><input type="text" class="form-control" id="harga_barang" name="harga_barang" disabled></td><td><input type="number" class="form-control" id="jumlah_barang" name="jumlah_barang" onchange="Hitung(this);" placeholder="Jumlah"></td><td><input type="text" class="form-control" id="jumlah_harga_barang" name="jumlah_harga_barang" disabled></td><td><button type="button" data-toggle="tooltip" title="" class="btn btn-outline-danger deleteform" data-original-title="Hapus"><i class="fa fa-trash"></i></button></td></tr>';
 
       $('.formtambah').append(html);
     });
@@ -122,21 +133,87 @@
 
   $('#id_barang').on('change', function() {
     const harga = $('#id_barang option:selected').data('harga');
-    const jumlah = $("#jumlah_barang").val();
+    alert(harga);
 
-    const total = harga * jumlah;
-
-    $('#jumlah_harga_barang').val(total);
+    $('#harga_barang').val(harga);
   });
 
-  $('#jumlah_barang').on('change', function() {
-    const harga = $('#id_barang option:selected').data('harga');
-    const jumlah = $("#jumlah_barang").val();
+  function Hitung(v) {
+    var index = $(v).parent().parent().index();
 
-    const total = harga * jumlah;
+    var harga = document.getElementsByName("harga_barang")[index].value;
+    var jumlah = document.getElementsByName("jumlah_barang")[index].value;
+    var jumlah_harga = harga * jumlah;
 
-    $('#jumlah_harga_barang').val(total);
-  });
+    document.getElementsByName('jumlah_harga_barang')[index].value = jumlah_harga;
+  }
+
+  // function Hitung(v) {
+  //   var index = $(v).parent().parent().index();
+
+  //   // var select = document.getElementById("id_barang").length;;
+  //   // alert(select);
+  //   // var barang = document.getElementById('id_barang')[0];
+  //   var barang = document.getElementById('id_barang');
+  //   // var harga = barang.getAttribute('data-harga');
+  //   // alert(barang);
+  //   var harga = barang.selectedIndex;
+  //   // alert(harga);
+  //   var harga_barang = barang.options[harga].getAttribute('data-harga');
+  //   // alert(harga_barang);
+  //   // alert(harga);
+  //   var jumlah = document.getElementsByName("jumlah_barang")[index].value;
+  //   // alert(jumlah);
+
+  //   var jumlah_harga = harga_barang * jumlah;
+
+  //   // $('#jumlah_harga_barang')[index].val(total);
+
+
+  //   // var harga = document.getElementsByName('#id_barang option:selected')[index].value;
+  //   // var jumlah = document.getElementsByName('jumlah_barang')[index].value;
+
+  //   // var jumlah_harga = harga * jumlah;
+  //   document.getElementsByName('jumlah_harga_barang')[index].value = jumlah_harga;
+  // }
+
+  // $(document).ready(function(e) {
+  //   $('#id_barang').on('change', function() {
+  //     const harga = $('#id_barang option:selected').data('harga');
+  //     const jumlah = $("#jumlah_barang").val();
+
+  //     const total = harga * jumlah;
+
+  //     $('#jumlah_harga_barang').val(total);
+  //   });
+
+  //   $('#jumlah_barang').on('change', function() {
+  //     const harga = $('#id_barang option:selected').data('harga');
+  //     const jumlah = $("#jumlah_barang").val();
+
+  //     const total = harga * jumlah;
+
+  //     $('#jumlah_harga_barang').val(total);
+  //   });
+  // });
+
+  // $('#id_barang').on('change', function() {
+  //   const harga = $('#id_barang option:selected').data('harga');
+  //   const jumlah = $("#jumlah_barang").val();
+
+  //   const total = harga * jumlah;
+
+  //   $('#jumlah_harga_barang').val(total);
+  // });
+
+  // $('#jumlah_barang').on('change', function() {
+  //   const harga = $('#id_barang option:selected').data('harga');
+  //   const jumlah = $("#jumlah_barang").val();
+
+  //   const total = harga * jumlah;
+
+  //   $('#jumlah_harga_barang').val(total);
+  // });
 
   <?php if (session()->getFlashdata('gagal_tambah') != NULL) { ?>
     Swal.fire({
